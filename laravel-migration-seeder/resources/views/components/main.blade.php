08a03c1e55544964aafbaa832fde1c420621e85a
Ncesecrede <francescopm95@gmail.com>
2021-09-10 14:04:01 +0100

    Trips

--- laravel-migration-seeder/resources/views/components/main.blade.php
+++ laravel-migration-seeder/resources/views/components/main.blade.php
@@ -2,7 +2,12 @@
     <div class="row">
         @foreach($allTrips as $trip)
             <div class="card-container">
-                
+                <div class="card-content">
+                    <div class="city">{{$trip->city}}</div>
+                    <img src="{{$trip->cover}}" alt="">
+                </div>
+                <div class="price"></div>
+                <div class="country"></div>
             </div>
         @endforeach
     </div>
